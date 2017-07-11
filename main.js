44fc65e0427d0315d33255066aa87f5780fd68c6
Pius Wong <pius@pioslabs.com>
2017-07-11 00:32:24 -0500

    Add map with Google Static Maps API

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
-
-  // get location
-  getLocation();
-
-
-  var keyGoogle = config.KEYGOOGLE;
-
-  // get weather
-  var keyDarkSky = config.KEYDARKSKY;
-
+  $("#start").on("click", function(){
+    // get location
+    var coordinates = getLocation();
+  })
 });
 
 // get location data
 function getLocation() {
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-      $("#location").innerHTML = "Geolocation is not supported by this browser.";
+    $("#city").innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 // show location
 function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" +
-      latlon + "&zoom=14&size=400x300&sensor=false&key=" + keyGoogle;
-    $("#map").innerHTML = "<img src='"+img_url+"'>";
-    $("#city").innerHTML = latlon;
-    console.log(latlon);
+  var keyGoogleStatic = config.KEYGOOGLESTATICMAPS;
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+  var mapsrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7C" + latlon + "&key=" + keyGoogleStatic;
+  $("#map").html("<img src='" + mapsrc + "'>");
+  // get weather
+  getWeather(position.coords);
+}
+// get weather
+function getWeather(coords){
+  var keyDarkSky = config.KEYDARKSKY;
+  console.log("lat/lon = " + coords.latitude + " " + coords.longitude); // debug
 }
