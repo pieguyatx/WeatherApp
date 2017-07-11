01fd9b94b21d632875355c82a9f20ea820cb6bf4
Pius Wong <pius@pioslabs.com>
2017-07-11 16:52:47 -0500

    Add wind direction and speed detection

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function getLocation() {
     $("#city").innerHTML("Geolocation is not supported by this browser.");
   }
 }
+
 // show location
 function showPosition(position) {
   // get map
@@ -32,6 +33,7 @@ function showPosition(position) {
   // get weather
   getWeather(latlon);
 }
+
 // get weather data
 function getWeather(latlon){
   console.log(latlon); // debug
@@ -47,10 +49,20 @@ function getWeather(latlon){
   var prefixCORS = "https://cors-anywhere.herokuapp.com/"; // see https://github.com/Rob--W/cors-anywhere
   var weatherAPI = prefixCORS + "https://api.darksky.net/forecast/" + keyDarkSky + "/" + latlon + "?units=" + unitSystem;
   $.getJSON(weatherAPI, function(weatherData){
+    // display temp
     var temp = Math.round(10*parseFloat(weatherData.currently.temperature))/10;
     $("#tempNo").html(temp);
     $("#tempUnit").html("&deg;"+units.temp)
+    // display wind values
     var wind = weatherData.currently.windSpeed;
+    $("#speed").html(wind);
+    $("#windUnit").html(units.speed);
+    var windBearing = parseInt(weatherData.currently.windBearing);
+    var windDirection = getDirection(windBearing);
+    console.log(windBearing);
+    console.log(windDirection);
+    $("#direction").html(windDirection);
+    // get other weather data for icons
     var cloudiness = weatherData.currently.cloudCover;
     var precipProb = weatherData.currently.precipProbability;
     var stormDist = weatherData.currently.nearestStormDistance;
@@ -58,6 +70,28 @@ function getWeather(latlon){
     var icon = weatherData.currently.icon;
   })
 
+}
+
+function getDirection(bearing){
+  var direction = "?";
+  if(bearing<=10 || bearing>=360){direction = "N"}
+  else if(bearing>10 && bearing<30){direction = "NNW"}
+  else if(bearing>=30 && bearing<=60){direction = "NW"}
+  else if(bearing>60 && bearing<80){direction = "NWW"}
+  else if(bearing>=80 && bearing<=100){direction = "W"}
+  else if(bearing>100 && bearing<120){direction = "SWW"}
+  else if(bearing>=120 && bearing<=150){direction = "SW"}
+  else if(bearing>150 && bearing<170){direction = "SSW"}
+  else if(bearing>=170 && bearing<=190){direction = "S"}
+  else if(bearing>190 && bearing<210){direction = "SSE"}
+  else if(bearing>=210 && bearing<=240){direction = "SE"}
+  else if(bearing>240 && bearing<260){direction = "SEE"}
+  else if(bearing>=260 && bearing<=280){direction = "E"}
+  else if(bearing>280 && bearing<300){direction = "NEE"}
+  else if(bearing>=300 && bearing<=330){direction = "NE"}
+  else if(bearing>330 && bearing<350){direction = "NNE"}
+  return direction;
+  console.log(direction);
 }
 
 // show weather icons
