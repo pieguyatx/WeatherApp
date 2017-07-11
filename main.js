cdb4d37032947d029a37d20deae984e723266f2e
Pius Wong <pius@pioslabs.com>
2017-07-11 00:58:18 -0500

    Get city/state from reverse geocoding Google API

--- main.js
+++ main.js
@@ -15,10 +15,17 @@ function getLocation() {
 }
 // show location
 function showPosition(position) {
+  // get map
   var keyGoogleStatic = config.KEYGOOGLESTATICMAPS;
   var latlon = position.coords.latitude + "," + position.coords.longitude;
-  var mapsrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7C" + latlon + "&key=" + keyGoogleStatic;
-  $("#map").html("<img src='" + mapsrc + "'>");
+  var mapAPI = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7C" + latlon + "&key=" + keyGoogleStatic;
+  $("#map").html("<img src='" + mapAPI + "'>");
+  // get city, state
+  var keyGoogleGeo = config.KEYGOOGLEGEOCODING;
+  var cityAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlon + "&result_type=locality&key=" + keyGoogleGeo;
+  $.getJSON(cityAPI, function(data){
+    console.log(data.results[0].formatted_address);
+  });
   // get weather
   getWeather(position.coords);
 }
